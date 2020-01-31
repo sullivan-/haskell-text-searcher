ea755b99915c91e9a884bc638db734fee7f2778d
John Sullivan <john.sullivan.mscs@protonmail.com>
2020-01-30 22:49:39 -0600

    add test suite scaffolding

--- test/TestTextSearcher.hs
+++ test/TestTextSearcher.hs
@@ -1,7 +1,40 @@
 module Main where
 
-import TextSearcher
+import qualified TextSearcher as TS
 import Test.HUnit
 
+data SearchTest = SearchTest {
+  testName :: String,
+  term :: String,
+  context :: Int,
+  expected :: [String]
+} deriving (Show)
+
+data FileTest = FileTest {
+  filename :: String,
+  searches :: [SearchTest]
+} deriving (Show)
+
+testSearch :: TS.SearchData -> SearchTest -> IO ()
+testSearch searchData searchTest =
+  let actual = TS.search searchData (term searchTest) (context searchTest)
+  in assertEqual (testName searchTest) (expected searchTest) actual
+
+testFile :: FileTest -> IO ()
+testFile fileTest = do
+  searchData <- TS.searchDataFromFile (filename fileTest)
+  sequence_ $ fmap (testSearch searchData) (searches fileTest)
+
+fileTests :: [FileTest]
+fileTests = [
+  FileTest "files/long_excerpt.txt"
+    [
+    ],
+  FileTest "files/short_excerpt.txt"
+    [
+      SearchTest "testOneHitNoContext" "sketch" 0 ["sketch"]
+    ]
+  ]
+
 main :: IO ()
-main = assertEqual "dummy" 0 0
+main = sequence_ $ fmap testFile fileTests
